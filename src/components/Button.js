97d5fd5c967a96845720fd6e9d48ffe0018e9ded
Blagoy Vangelov <blagoy.vangelov@emerchantpay.com>
2019-09-09 14:16:39 +0300

    remove commented code

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,5 @@
 import React from 'react'
 
-// export const Button = ({text, action, data_id}) => {
-// 	return(
-// 		<button onClick={action} data-id={data_id}>{text}</button>
-// 	);
-// }
-
 export const Button = function(props) {
 	return(
 		<button onClick={props.action} data-id={props.data_id}>{props.text}</button>
